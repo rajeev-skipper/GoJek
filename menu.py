a5f63b418323280c6db92b07b752b994e8ee69a6
rajeev-skipper <81459086+rajeev-skipper@users.noreply.github.com>
2021-11-15 17:01:07 +0530

    create

---
+++ menu.py
@@ -0,0 +1,26 @@
+class menu:
+    def takeuserinput(self,raj):
+        print('Select an option:'),
+        print(str(1)+'.'+'Credit'),
+        print(str(2)+'.'+'Debit'),
+        print(str(3)+'.'+'Check Balance'),
+        print(str(4)+'.'+'Exit')
+        
+        while True:
+            n = int(input())
+            if n not in [1,2,3,4]:
+                print('Invalid Input')
+                exit()
+            inpt = n
+            if inpt==1:
+                raj.credit()
+                #credit(balance)
+            if inpt==2:
+                raj.debit()
+                #debit(balance)
+            if inpt == 3:
+                raj.checkbalance()
+                #checkbalance(balance)
+            if inpt==4:
+                exit()
+                break
